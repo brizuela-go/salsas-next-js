8ab60e181c8908ec8d29bb3451bc4239898e9ae3
Juan Brizuela <juanbrizuelago@gmail.com>
2022-05-30 18:20:00 -0500

    changed responsiveness

--- pages/recipes/[slug].js
+++ pages/recipes/[slug].js
@@ -57,14 +57,17 @@ export default function RecipeDetails({ recipe }) {
       <Head>
         <title>Las Salsas | {title}</title>
       </Head>
-
       <img className="mask" src={"https:" + featuredImage.fields.file.url} />
       <div className="mask" />
-
+      <div className="mb-14 mb-lg-12 mb-md-14 mb-sm-11 " />
       <h2 className="text-white text-center display-1 fw-bolder salsa-title">
         {title}
       </h2>
-
+      <div className="mb-14 mb-lg-10 mb-md-14 " />
+      ‎
+      <div className="mb-sm-9 mb-12 mb-lg-0 mb-md-14" />
+      ‎
+      <div className="mb-md-12" />
       <div className="info">
         <h4 className="mb-4">
           <i className="fas fa-stopwatch fa-lg "></i>
@@ -85,7 +88,6 @@ export default function RecipeDetails({ recipe }) {
           // <span key={ing}>{ing}</span>
         ))}
       </div>
-
       <div className="method mt-5">
         <div>{documentToReactComponents(method)}</div>
       </div>
